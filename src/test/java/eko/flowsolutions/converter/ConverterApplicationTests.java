6c34de3af405123ef8d082ce08c52b8e8ea43900
Eko Nugroho <ekonugroho93.en@gmail.com>
2020-03-05 14:37:34 +0900

    Initial commit

---
+++ src/test/java/eko/flowsolutions/converter/ConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package eko.flowsolutions.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
